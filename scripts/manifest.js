8e2c0558571b1cebb015ada9cc9e460e3925cb02
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-10-22 17:06:59 -0500

    issues and pr

--- scripts/manifest.js
+++ scripts/manifest.js
@@ -2,9 +2,12 @@ const { writeFileSync } = require('fs');
 const { join } = require('path');
 const chalk = require('chalk');
 
-const createManifest = async () => {
+const css = ['styles/repo.css', 'styles/profile.css', 'styles/readme.css', 'styles/home.css', 'styles/explore.css', 'styles/issue.css', 'styles/globals.css'];
+const js = ['inject.js'];
+
+const createManifest = async ({ js, css }) => {
   try {
-    writeFileSync(join(__dirname, '..', 'dist', 'manifest.json'), `{"manifest_version":3,"version":"1.0.0","name":"github stellar theme","author":"frog1123","description":"stellar theme for github","permissions":["contextMenus","activeTab","scripting"],"content_scripts":[{"matches":["https://github.com/*","http://github.com/*"],"js":["inject.js"],"css":["styles/globals.css","styles/repo.css","styles/profile.css","styles/readme.css","styles/home.css","styles/explore.css"]}],"icons":{"16":"16.png","32":"32.png","48":"48.png","64":"64.png","128":"128.png"}}`);
+    writeFileSync(join(__dirname, '..', 'dist', 'manifest.json'), `{"manifest_version":3,"version":"1.0.0","name":"github stellar theme","author":"frog1123","description":"stellar theme for github","permissions":["contextMenus","activeTab","scripting"],"content_scripts":[{"matches":["https://github.com/*","http://github.com/*"],"js":${JSON.stringify(js)},"css":${JSON.stringify(css)}}],"icons":{"16":"16.png","32":"32.png","48":"48.png","64":"64.png","128":"128.png"}}`);
     console.log(`${chalk.green('✔')} created mainfest.json`);
   } catch (err) {
     console.log(err);
@@ -12,7 +15,7 @@ const createManifest = async () => {
   }
 };
 
-createManifest();
+createManifest({ js, css });
 
 // {
 //   "manifest_version": 3,
@@ -24,8 +27,8 @@ createManifest();
 //   "content_scripts": [
 //     {
 //       "matches": ["https://github.com/*", "http://github.com/*"],
-//       "js": ["inject.js"],
-//       "css": ["styles/globals.css", "styles/repo.css", "styles/profile.css", "styles/readme.css", "styles/home.css", "styles/explore.css"]
+//       "js": [],
+//       "css": []
 //     }
 //   ],
 //   "icons": {
