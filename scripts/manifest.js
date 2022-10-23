7e34a82358d940c548a15ddb171ab6bf219ddee5
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-10-23 18:52:25 -0500

    Update manifest.js

--- scripts/manifest.js
+++ scripts/manifest.js
@@ -2,12 +2,17 @@ const { writeFileSync } = require('fs');
 const { join } = require('path');
 const chalk = require('chalk');
 
-const css = ['styles/repo.css', 'styles/profile.css', 'styles/readme.css', 'styles/home.css', 'styles/explore.css', 'styles/issue.css', 'styles/pullreqs.css', 'styles/notifications.css', 'styles/accsettings.css', 'styles/commit.css', 'styles/globals.css'];
-const js = ['inject.js'];
+const contentScripts = [
+  {
+    matches: ['https://github.com/*', 'http://github.com/*'],
+    js: ['inject.js'],
+    css: ['styles/home.css', 'styles/repo.css', 'styles/profile.css', 'styles/readme.css', 'styles/explore.css', 'styles/issue.css', 'styles/pullreqs.css', 'styles/notifications.css', 'styles/accsettings.css', 'styles/commit.css']
+  }
+];
 
-const createManifest = async ({ js, css }) => {
+const createManifest = async ({ contentScripts }) => {
   try {
-    writeFileSync(join(__dirname, '..', 'dist', 'manifest.json'), `{"manifest_version":3,"version":"1.0.0","name":"github stellar theme","author":"frog1123","description":"stellar theme for github","permissions":["contextMenus","activeTab","scripting"],"content_scripts":[{"matches":["https://github.com/*","http://github.com/*"],"js":${JSON.stringify(js)},"css":${JSON.stringify(css)}}],"icons":{"16":"16.png","32":"32.png","48":"48.png","64":"64.png","128":"128.png"},"action":{"default_icon":{"16":"16.png","32":"32.png","48":"48.png","64":"64.png","128":"128.png"},"default_title":"github stellar theme","default_popup":"popup.html"}}`);
+    writeFileSync(join(__dirname, '..', 'dist', 'manifest.json'), `{"manifest_version":3,"version":"1.0.0","name":"github stellar theme","author":"frog1123","description":"stellar theme for github","permissions":["contextMenus","activeTab","scripting"],"content_scripts":${JSON.stringify(contentScripts)},"icons":{"16":"16.png","32":"32.png","48":"48.png","64":"64.png","128":"128.png"},"action":{"default_icon":{"16":"16.png","32":"32.png","48":"48.png","64":"64.png","128":"128.png"},"default_title":"github stellar theme","default_popup":"popup.html"}}`);
     console.log(`${chalk.green('✔')} created mainfest.json`);
   } catch (err) {
     console.log(err);
@@ -15,7 +20,7 @@ const createManifest = async ({ js, css }) => {
   }
 };
 
-createManifest({ js, css });
+createManifest({ contentScripts });
 
 // {
 //   "manifest_version": 3,
