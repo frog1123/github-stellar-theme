a0d728ed58d3823fd258003f088c4e77c4ebe2b1
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-10-23 14:38:13 -0500

    readme

--- src/inject.ts
+++ src/inject.ts
@@ -1,1 +1,9 @@
 console.log('%c 🪐| stellar theme injected', 'color: #9335f2');
+
+const readmeCode = document.querySelectorAll('#readme > div.Box-body.px-5.pb-5 > article > div > pre');
+
+// highlight readme code stuff
+for (let i = 0; i < readmeCode.length; i++) {
+  const firstWord = readmeCode[i].textContent?.split(' ').shift();
+  readmeCode[i].innerHTML = `<span class="stellar-injected-readme-code-syntax-highlight">${firstWord}</span>${readmeCode[i].textContent?.split('').slice(firstWord?.length).join('')}`;
+}
